5e9fe1b7b01f884d9c545f7380edd50d99a8f43d
Alfredo Artiles <aartiles@gmail.com>
2019-06-06 12:42:04 +0200

    Update Recurly whitelisted IPs

--- src/config.js
+++ src/config.js
@@ -14,6 +14,10 @@ module.exports = {
     profileURL: process.env.PROFILE_URL || 'https://recurly.com/accounts/{accountId}',
   },
   ipWhitelist: [
+    '35.233.168.62',
+    '35.185.253.62',
+    '35.188.232.138',
+    '35.236.210.191',
     '50.18.192.88',
     '52.8.32.100',
     '52.9.209.233',
