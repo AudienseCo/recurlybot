caa1b33c0f6515109aff05c39db2eb9f3db1ff11
Alfredo Artiles <aartiles@gmail.com>
2019-07-10 10:53:52 +0200

    Add different amount ranges gifs

--- src/presentation/newSubscriptionTemplate.js
+++ src/presentation/newSubscriptionTemplate.js
@@ -11,7 +11,7 @@ module.exports = ({
   gif
 }) => {
   const color = amount > 25 ? 'good' : '#ddddd';
-  const image = amount > 25 ? getMoneyGif(gif) : null;
+  const image = gif || getMoneyGif(amount);
   const attachment = {
     title: `New ${planName} subscription`,
     title_link: config.presentation.profileURL.replace('{accountId}', accountId),
@@ -46,9 +46,8 @@ module.exports = ({
   };
 };
 
-function getMoneyGif(gif) {
-  if (gif) return gif;
-  const GIFs = [
+function getMoneyGif(amount) {
+  const LEVEL1_GIFS = [
     'https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif',
     'https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif',
     'https://media.giphy.com/media/ND6xkVPaj8tHO/giphy.gif',
@@ -60,10 +59,20 @@ function getMoneyGif(gif) {
     'https://media.giphy.com/media/k4iuvHyjOVtzq/giphy.gif',
     'https://media.giphy.com/media/lptjRBxFKCJmFoibP3/giphy.gif',
     'https://media.giphy.com/media/xT8qAY7e9If38xkrIY/giphy.gif',
-    'https://media.giphy.com/media/yoJC2GnSClbPOkV0eA/giphy.gif',
+    'https://media.giphy.com/media/yoJC2GnSClbPOkV0eA/giphy.gif'
+  ];
+  const LEVEL2_GIFS = [
     'https://media.giphy.com/media/MdG0ZaSpScdl8VaJks/giphy.gif',
     'https://media.giphy.com/media/hTf8S01Rn6SOFFGBYc/giphy.gif',
-    'https://media.giphy.com/media/lN9BJEfmy1huhxiCEw/giphy.gif'
+    'https://media.giphy.com/media/lN9BJEfmy1huhxiCEw/giphy.gif',
+    'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/charli-xcx-boys-moments-5-1501166124.gif?crop=1xw:1xh;center,top&resize=480:*'
   ];
-  return sample(GIFs);
+  const LEVEL3_GIFS = [
+    'http://dwigif.com/view/O42mUTl'
+  ];
+
+  if (amount > 25 && amount < 300) return sample(LEVEL1_GIFS);
+  if (amount >= 300 && amount < 500) return sample(LEVEL2_GIFS);
+  if (amount >= 500) return sample(LEVEL3_GIFS);
+  return null;
 }
